6b05c7c82dfeabefd062463fbfdecadfe69513df
WareBar <barryadam615@gmail.com>
2025-01-04 10:23:14 +0800

    changed the database to POSTGRESQL

--- TODO_APP/settings.py
+++ TODO_APP/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'TODO_APP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '109709',
+        'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
+        'PORT': '5432',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
